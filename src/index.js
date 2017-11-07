f4d4bdb574447dc706b56c970185e9416bacb037
Jan T. Sott <jan@idleberg.com>
2017-11-07 02:09:23 +0100

    add verbose option

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { download } from './util';
 window.onload = () => {
     // Check File API support
     if (window.File && window.FileList && window.FileReader) {
+        console.log(`webvs-ui v${require('../package.json').version}`);
+
         const loadFiles = document.getElementById("loadFiles");
 
         loadFiles.addEventListener("change", (event) => {
             let files = event.target.files;
-            console.log( (files.length === 1) ? "Converting single file" : "Convert multiple files");
+            console.log( (files.length === 1) ? "\nConverting single file" : "\nConvert multiple files");
             download(files, true);
         });
     }
